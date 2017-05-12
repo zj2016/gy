9b0153079da3339ac1062b2f9d18f3d999ab11e7
jon <jonzhang@renrenbx.com>
2017-05-12 17:48:16 +0800

    修改返回数据结构

--- gy/src/main/java/com/bs/gy/controller/BaseController.java
+++ gy/src/main/java/com/bs/gy/controller/BaseController.java
@@ -7,58 +7,57 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bs.gy.query.Query;
 import com.bs.gy.rest.Rest;
-import com.bs.gy.rest.RestResult;
+import com.bs.gy.rest.RestO;
 import com.bs.gy.service.BaseService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 
 public class BaseController<T> {
 
 	@Autowired
 	private BaseService<T> service;
 	
-	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	public String list(Query query) throws JsonProcessingException{
+	@RequestMapping(value = "/list", method = RequestMethod.GET, produces = "text/json;charset=UTF-8")
+	public @ResponseBody String list(Query query){
 		Map<String,Object> params = query.toMap();
 		List<T> list = service.getList(params);
 		int count = service.getCount(params);
 		Rest<T> rest = new Rest<T>(count, list);
-		System.out.println(rest.toJson());
 		return rest.toJson();
 	}
 	
-	@RequestMapping(value = "/add", method = RequestMethod.POST)
-	public RestResult add(T active){
+	@RequestMapping(value = "/add", method = RequestMethod.POST, produces = "text/json;charset=UTF-8")
+	public @ResponseBody String add(T active){
 		int result = service.add(active);
 		if(result > 0){
-			return RestResult.success();
+			return Rest.success().toJson();
 		}
-		return RestResult.error("添加成功");
+		return Rest.error("添加成功").toJson();
 	}
 	
-	@RequestMapping(value = "/update", method = RequestMethod.POST)
-	public RestResult update(T active){
+	@RequestMapping(value = "/update", method = RequestMethod.POST, produces = "text/json;charset=UTF-8")
+	public @ResponseBody String update(T active){
 		int result = service.update(active);
 		if(result > 0){
-			return RestResult.success();
+			return Rest.success().toJson();
 		}
-		return RestResult.error("更新成功");
+		return Rest.error("更新成功").toJson();
 	}
 	
-	@RequestMapping(value = "/remove/{id}")
-	public RestResult remove(@PathVariable("id") Integer id){
+	@RequestMapping(value = "/remove/{id}", produces = "text/json;charset=UTF-8")
+	public @ResponseBody String remove(@PathVariable("id") Integer id){
 		int result = service.remove(id);
 		if(result > 0){
-			return RestResult.success();
+			return Rest.success().toJson();
 		}
-		return RestResult.error("删除成功");
+		return Rest.error("删除成功").toJson();
 	}
 	
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public RestResult one(@PathVariable("id") Integer id){
-		return RestResult.success().setResponse(service.get(id));
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "text/json;charset=UTF-8")
+	public @ResponseBody String one(@PathVariable("id") Integer id){
+		return RestO.success().setData(service.get(id)).toJson();
 	}
 	
 }
--- gy/src/main/java/com/bs/gy/rest/RestO.java
+++ gy/src/main/java/com/bs/gy/rest/RestO.java
@@ -1,53 +1,52 @@
 package com.bs.gy.rest;
 
-import java.util.List;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class Rest<T> {
+public class RestO<T> {
 	
 	public static final Integer SUCCESS = 200;
 	
 	public static final Integer FAIL = 500;
 	
 	private int total;
-	private List<T> data;
+	private T data;
 	private Integer code;
 	private String message;
 	
 	public int getTotal() {
 		return total;
 	}
-	public Rest<T> setTotal(int total) {
+	public RestO<T> setTotal(int total) {
 		this.total = total;
 		return this;
 	}
-	public List<T> getData() {
+	public T getData() {
 		return data;
 	}
-	public Rest<T> setData(List<T> data) {
+	public RestO<T> setData(T data) {
 		this.data = data;
 		return this;
 	}
 	public Integer getCode() {
 		return code;
 	}
-	public Rest<T> setCode(Integer code) {
+	public RestO<T> setCode(Integer code) {
 		this.code = code;
 		return this;
 	}
 	public String getMessage() {
 		return message;
 	}
-	public Rest<T> setMessage(String message) {
+	public RestO<T> setMessage(String message) {
 		this.message = message;
 		return this;
 	}
-	public Rest() {
+	public RestO() {
 		super();
 	}
-	public Rest(int total, List<T> data) {
+	public RestO(int total, T data) {
 		super();
 		this.total = total;
 		this.data = data;
@@ -62,20 +61,20 @@ public class Rest<T> {
 		return "error";
 	}
 	
-	public Rest(Integer code, String message){
+	public RestO(Integer code, String message){
 		this.code = code;
 		this.message = message;
 	}
 	
-	public static Rest error(Integer code, String message){
-		return new Rest(code, message);
+	public static RestO error(Integer code, String message){
+		return new RestO(code, message);
 	}
 	
-	public static Rest error(String message){
+	public static RestO error(String message){
 		return error(FAIL,message);
 	}
 	
-	public static Rest success(){
-		return new Rest(SUCCESS, "success");
+	public static RestO success(){
+		return new RestO(SUCCESS, "success");
 	}
 }
