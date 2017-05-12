6e3de8131b400cdcbbd21ba6c9a342c476c1ee05
jon <jonzhang@renrenbx.com>
2017-05-12 18:24:27 +0800

    学校大查询

---
+++ gy/src/main/java/com/bs/gy/dao/SchoolAllMapper.java
@@ -0,0 +1,6 @@
+package com.bs.gy.dao;
+
+import com.bs.gy.vo.SchoolAll;
+
+public interface SchoolAllMapper  extends BaseMapper<SchoolAll>{
+}
---
+++ gy/src/main/java/com/bs/gy/vo/SchoolAll.java
@@ -0,0 +1,188 @@
+package com.bs.gy.vo;
+
+import java.util.Date;
+
+public class SchoolAll {
+
+	private Integer ids;
+
+    private String code;
+
+    private String name;
+
+    private String provicename;
+
+    private String cityname;
+
+    private Integer academician;
+
+    private Integer doctor;
+
+    private Integer master;
+
+    private Date createtime;
+
+    private String address;
+
+    private String description;
+
+    private String logoimg;
+
+    private String rangename;
+
+    private String naturename;
+
+    private String levelname;
+
+    private String batchname;
+
+    private String extra1;
+
+    private String extra2;
+
+	public Integer getIds() {
+		return ids;
+	}
+
+	public void setIds(Integer ids) {
+		this.ids = ids;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getProvicename() {
+		return provicename;
+	}
+
+	public void setProvicename(String provicename) {
+		this.provicename = provicename;
+	}
+
+	public String getCityname() {
+		return cityname;
+	}
+
+	public void setCityname(String cityname) {
+		this.cityname = cityname;
+	}
+
+	public Integer getAcademician() {
+		return academician;
+	}
+
+	public void setAcademician(Integer academician) {
+		this.academician = academician;
+	}
+
+	public Integer getDoctor() {
+		return doctor;
+	}
+
+	public void setDoctor(Integer doctor) {
+		this.doctor = doctor;
+	}
+
+	public Integer getMaster() {
+		return master;
+	}
+
+	public void setMaster(Integer master) {
+		this.master = master;
+	}
+
+	public Date getCreatetime() {
+		return createtime;
+	}
+
+	public void setCreatetime(Date createtime) {
+		this.createtime = createtime;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getLogoimg() {
+		return logoimg;
+	}
+
+	public void setLogoimg(String logoimg) {
+		this.logoimg = logoimg;
+	}
+
+	public String getRangename() {
+		return rangename;
+	}
+
+	public void setRangename(String rangename) {
+		this.rangename = rangename;
+	}
+
+	public String getNaturename() {
+		return naturename;
+	}
+
+	public void setNaturename(String naturename) {
+		this.naturename = naturename;
+	}
+
+	public String getLevelname() {
+		return levelname;
+	}
+
+	public void setLevelname(String levelname) {
+		this.levelname = levelname;
+	}
+
+	public String getBatchname() {
+		return batchname;
+	}
+
+	public void setBatchname(String batchname) {
+		this.batchname = batchname;
+	}
+
+	public String getExtra1() {
+		return extra1;
+	}
+
+	public void setExtra1(String extra1) {
+		this.extra1 = extra1;
+	}
+
+	public String getExtra2() {
+		return extra2;
+	}
+
+	public void setExtra2(String extra2) {
+		this.extra2 = extra2;
+	}
+
+    
+}
